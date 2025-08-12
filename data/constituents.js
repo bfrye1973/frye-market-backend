bb6abd08b38634f4ea5b17bdab7d66738e34b24e
bfrye1973 <brianfrye23@gmail.com>
2025-08-12 04:27:42 -0700

    constituents.js

---
+++ data/constituents.js
@@ -0,0 +1,6 @@
+// data/constituents.js
+// Minimal stub: return an empty sectors object.
+// Your real version can return {TECH: [...], FIN: [...], ...}
+export default function loadSectors() {
+  return {};
+}
