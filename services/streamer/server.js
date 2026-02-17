8b99ea921babe645f581bcc85b2dbb2db2702fd7
bfrye1973 <brianfrye23@gmail.com>
2026-02-17 09:09:00 -0700

    Update server.js

--- services/streamer/server.js
+++ services/streamer/server.js
@@ -3,6 +3,8 @@ import express from "express";
 import streamRouter from "./routes/stream.js";
 import scalpRouter from "./routes/scalp.js";
 import { startEngine5B } from "./engine5b/runner.js";
+import { pushoverTestRouter } from "./routes/pushoverTest.js";
+
 
 const app = express();
 
@@ -24,8 +26,9 @@ const ALLOWED_ORIGINS = new Set([
   "http://localhost:3000",
 ]);
 
-app.use((req, res, next) => {
+app.use((req, res, next) => {   
   const origin = req.headers.origin;
+ 
 
   if (origin && ALLOWED_ORIGINS.has(origin)) {
     // MUST match exactly for EventSource
@@ -65,6 +68,7 @@ app.use("/stream", streamRouter);
    Engine 5B routes
 -------------------------------------------------- */
 app.use("/stream", scalpRouter);
+app.use("/api/v1", pushoverTestRouter);
 
 /* -------------------------------------------------
    Background runner
