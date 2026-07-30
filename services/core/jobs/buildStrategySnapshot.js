8d65fa2ca42058c4d417a2702583de1547866031
bfrye1973 <brianfrye23@gmail.com>
2026-07-29 17:03:53 -0700

    Update buildStrategySnapshot.js

--- services/core/jobs/buildStrategySnapshot.js
+++ services/core/jobs/buildStrategySnapshot.js
@@ -2837,7 +2837,7 @@ function buildEngine6PaperPermission({
     paperAllowed:
       allowed === true,
 
-    planningAllowed:
+    planningAllowed:6
       allowed === true,
 
     ticketAllowed:
@@ -2853,10 +2853,20 @@ function buildEngine6PaperPermission({
       fastIntradayPaperAllow === true,
 
     engine15Bypassed:
-      engine15FastLaneExcluded === true,
+      engine15FastLaneExcluded:
+        engine15FastLaneExcluded === true,
 
-    engine15FastLaneExcluded:
-      engine15FastLaneExcluded === true,
+      authority:
+        engine15FastLaneExcluded === true
+          ? "Engine 6 Fast Lane"
+          : "Engine 15",
+
+      authoritySource:
+        engine15FastLaneExcluded === true
+          ? "ENGINE6_FAST_LANE_PERMISSION_AUTHORITY"
+          : "ENGINE15_HIGHER_DEGREE_READINESS_AUTHORITY",
+
+       engine15SuppressedArtifacts: {
 
     engine15SuppressedArtifacts: {
       blockers:
