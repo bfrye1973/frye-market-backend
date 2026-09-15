a8b204034316e2c55d2c5143fe6da727857111f8
bfrye1973 <brianfrye23@gmail.com>
2026-09-15 15:55:59 -0700

    Update validateFreshness.js

--- services/core/logic/engine29/data/validateFreshness.js
+++ services/core/logic/engine29/data/validateFreshness.js
@@ -5,7 +5,9 @@ const HOUR_MS = 60 * 60 * 1000;
 export const ENGINE29_FRESHNESS_DEFAULTS = Object.freeze({
   DAILY_MAX_AGE_MS: 96 * HOUR_MS,
   HOURLY_MAX_AGE_MS: 36 * HOUR_MS,
+  THIRTY_MIN_MAX_AGE_MS: 18 * HOUR_MS,
   WEEKEND_HOURLY_MAX_AGE_MS: 84 * HOUR_MS,
+  WEEKEND_THIRTY_MIN_MAX_AGE_MS: 84 * HOUR_MS,
 });
 
 function isWeekendUtc(nowMs) {
@@ -19,7 +21,9 @@ export function evaluateFreshness({
   now = Date.now(),
   dailyMaxAgeMs = ENGINE29_FRESHNESS_DEFAULTS.DAILY_MAX_AGE_MS,
   hourlyMaxAgeMs = ENGINE29_FRESHNESS_DEFAULTS.HOURLY_MAX_AGE_MS,
+  thirtyMinMaxAgeMs = ENGINE29_FRESHNESS_DEFAULTS.THIRTY_MIN_MAX_AGE_MS,
   weekendHourlyMaxAgeMs = ENGINE29_FRESHNESS_DEFAULTS.WEEKEND_HOURLY_MAX_AGE_MS,
+  weekendThirtyMinMaxAgeMs = ENGINE29_FRESHNESS_DEFAULTS.WEEKEND_THIRTY_MIN_MAX_AGE_MS,
 } = {}) {
   const latestMs = Number(latestTime);
 
@@ -38,6 +42,8 @@ export function evaluateFreshness({
 
   if (timeframe === "1H") {
     maxAgeMs = isWeekendUtc(now) ? weekendHourlyMaxAgeMs : hourlyMaxAgeMs;
+  } else if (timeframe === "30m") {
+    maxAgeMs = isWeekendUtc(now) ? weekendThirtyMinMaxAgeMs : thirtyMinMaxAgeMs;
   }
 
   return {
