01a026714b58e74b473f6c083c0e35bfabb52160
bfrye1973 <brianfrye23@gmail.com>
2026-09-15 16:00:31 -0700

    Update buildLeadershipGroup.js

--- services/core/logic/engine29/groups/buildLeadershipGroup.js
+++ services/core/logic/engine29/groups/buildLeadershipGroup.js
@@ -4,6 +4,7 @@ import { ENGINE29_GROUP_IDS, ENGINE29_GROUP_STATES } from "../constants.js";
 import { ENGINE29_REASON_CODES } from "../canonical/reasonCodes.js";
 import {
   groupBase,
+  timeframeLabel,
   isBreakingOrWorse,
   isConfirmedBreak,
   isRecovering,
@@ -66,7 +67,7 @@ function buildOne(symbols, timeframeKey) {
 
   return groupBase({
     group: ENGINE29_GROUP_IDS.LEADERSHIP,
-    timeframe: timeframeKey === "tactical" ? "1H" : "1W",
+    timeframe: timeframeLabel(timeframeKey),
     state,
     members,
     subgroups: {
@@ -80,5 +81,9 @@ function buildOne(symbols, timeframeKey) {
 }
 
 export function buildLeadershipGroup(symbols = {}) {
-  return { structural: buildOne(symbols, "structural"), tactical: buildOne(symbols, "tactical") };
+  return {
+    structural: buildOne(symbols, "structural"),
+    tactical: buildOne(symbols, "tactical"),
+    fastTactical: buildOne(symbols, "fastTactical"),
+  };
 }
