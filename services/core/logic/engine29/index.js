36289a91744c92ab1953a9be9d9f3c600b1c4880
bfrye1973 <brianfrye23@gmail.com>
2026-09-15 14:28:39 -0700

    Update index.js

--- services/core/logic/engine29/index.js
+++ services/core/logic/engine29/index.js
@@ -4,3 +4,6 @@ export * from "./constants.js";
 export * from "./symbolRegistry.js";
 export * from "./canonical/reasonCodes.js";
 export * from "./canonical/validateEngine29Contract.js";
+export * from "./data/normalizeMarketBars.js";
+export * from "./data/validateFreshness.js";
+export * from "./data/buildMarketDataBundle.js";
