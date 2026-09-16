0b8ef00065a9788f07b77df515f0de171de7b72c
bfrye1973 <brianfrye23@gmail.com>
2026-09-15 22:49:29 -0700

    Update index.js

--- services/core/logic/engine29/index.js
+++ services/core/logic/engine29/index.js
@@ -4,9 +4,11 @@ export * from "./constants.js";
 export * from "./symbolRegistry.js";
 export * from "./canonical/reasonCodes.js";
 export * from "./canonical/validateEngine29Contract.js";
+
 export * from "./data/normalizeMarketBars.js";
 export * from "./data/validateFreshness.js";
 export * from "./data/buildMarketDataBundle.js";
+
 export * from "./structure/aggregateDailyToWeekly.js";
 export * from "./structure/calculateEma.js";
 export * from "./structure/detectSwingStructure.js";
@@ -14,6 +16,7 @@ export * from "./structure/deriveSupportResistance.js";
 export * from "./structure/classifySymbolStructure.js";
 export * from "./structure/buildSymbolStructure.js";
 export * from "./structure/buildStructureBundle.js";
+
 export * from "./groups/groupUtils.js";
 export * from "./groups/buildHeadlineIndexGroup.js";
 export * from "./groups/buildBreadthGroup.js";
@@ -24,12 +27,14 @@ export * from "./groups/buildEnergyInflationGroup.js";
 export * from "./groups/buildVolatilityGroup.js";
 export * from "./groups/buildFinancialConditionsGroup.js";
 export * from "./groups/buildGroupStateBundle.js";
+
 export * from "./tacticalCharacter/moveCharacterConstants.js";
 export * from "./tacticalCharacter/tacticalCharacterUtils.js";
 export * from "./tacticalCharacter/buildEsFuturesAnchor.js";
 export * from "./tacticalCharacter/detectLiquiditySweep.js";
 export * from "./tacticalCharacter/detectFailedMove.js";
 export * from "./tacticalCharacter/detectBroadConfirmation.js";
+export * from "./tacticalCharacter/detectUnderlyingPressure.js";
 export * from "./tacticalCharacter/detectSqueezeCharacter.js";
 export * from "./tacticalCharacter/resolveMoveCharacter.js";
 export * from "./tacticalCharacter/buildTacticalCharacter.js";
